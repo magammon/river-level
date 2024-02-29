21af17f42764f12e6347feb8949578422bf9eee2
magammon <michael.gumn@googlemail.com>
2024-02-29 14:33:15 +0000

    loads of changes.

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM debian:bookworm
+
+EXPOSE 8897
+## update apt, install python3, pip and venv
+RUN apt-get update && apt-get install -y python3 python3-pip && apt install -y python3-venv
+## make directory for the venv
+RUN mkdir /env
+## make the venv in /env directory
+RUN python3 -m venv /env
+## copy the minimum requirements file for 
+COPY minrequirements.txt /
+## using the pip in the minimum requirements for the script
+RUN /env/bin/pip install -r minrequirements.txt
+## copy over the python script
+COPY riverlevel.py /
+## Using the python 3 in the venv execute the script
+CMD /env/bin/python3 riverlevel.py
---
+++ riverlevel.py
@@ -0,0 +1,43 @@
+import json
+import time
+import requests as rq
+# Import Gauge and start_http_server from prometheus_client
+from prometheus_client import Gauge, start_http_server
+
+## set read interval
+READ_INTERVAL = 15
+
+## set read interval units. 1 = seconds, 60 = minutes, 3600 = hours, 86400 = days
+READ_UNITS = 1
+
+## set api uri
+API_URI = "https://environment.data.gov.uk/flood-monitoring/id/measures/531160-level-stage-i-15_min-mASD.json"
+
+## initialise the gauges
+
+gauge_river_level = Gauge('keynsham_river_level', 'River level at Keynsham Rivermeads')
+
+## define function jprint which makes the json output look pretty and easy to understand
+def jprint(obj):
+    """Function takes api output from EA API and returns river level as float."""
+    height = json.dumps(obj['items']['latestReading']['value'])
+    return float(height)
+
+## define function getheight. Function calls API and then sets prometheus guage
+def getheight():
+    """Function calls API, feeds to jprint and then sets prometheus guage."""
+    ## get response
+    response = rq.get(API_URI)
+    ## set river guage river level to output of jprint function
+    gauge_river_level.set(jprint(response.json()))
+    print(gauge_river_level)
+    time.sleep(READ_INTERVAL * READ_UNITS)
+
+if __name__ == "__main__":
+    #expose metrics
+    METRICS_PORT = 8897
+    start_http_server(METRICS_PORT)
+    print("Serving sensor metrics on :{}".format(METRICS_PORT))
+
+    while True:
+        getheight()
