803aa68d1bc167d6d7ba37da2374515534db537d
magammon <michael.gumn@googlemail.com>
2024-03-06 22:51:34 +0000

    fixed issues with naming station programatically.

--- riverlevel.py
+++ riverlevel.py
@@ -9,44 +9,44 @@ import requests as rq
 # Import Gauge and start_http_server from prometheus_client
 from prometheus_client import Gauge, start_http_server
 
-## set read units. 1 = seconds, 60 = minutes, 3600 = hours, 86400 = days
+# set read units. 1 = seconds, 60 = minutes, 3600 = hours, 86400 = days
 READ_UNITS = 60
 
-## set read interval of how many multiples of the read units between scrapes of the API
+# set read interval of how many multiples of the read units between scrapes of the API
 READ_INTERVAL = 1
 
-## set api uris. These are set as docker environment variables
-MEASURE_API = os.environ['MEASURE_API']
+# set api uris. These are set as docker environment variables
+#MEASURE_API = os.environ['MEASURE_API']
 
-STATION_API = os.environ['STATION_API']
+#STATION_API = os.environ['STATION_API']
 
-## set api uris for testing. comment out when building
+# set api uris for testing. comment out when building
 
-## define function get_height which makes the json output look pretty and easy to understand
+MEASURE_API = "https://environment.data.gov.uk/flood-monitoring/id/measures/531160-level-stage-i-15_min-mASD.json"
+
+STATION_API = "https://environment.data.gov.uk/flood-monitoring/id/stations/531160.json"
+
+# define functions
+def get_station_name(obj):
+    """Function takes api output from EA API and returns name of station as string."""
+    stationname = json.dumps(obj['items']['label'])
+    return stationname.replace('"','')
+
 def get_height(obj):
     """Function takes api output from EA API and returns river level as float."""
     height = json.dumps(obj['items']['latestReading']['value'])
     return float(height)
 
-## define function get_station_name which makes the json output look pretty and easy to understand
-def get_station_name(obj):
-    """Function takes api output from EA API and returns name of station."""
-    recordmax = json.dumps(obj['items']['label'])
-    return float(recordmax)
-
-## define function get_typical which makes the json output look pretty and easy to understand
 def get_typical(obj):
     """Function takes api output from EA API and returns information about station."""
     typical = json.dumps(obj['items']['stageScale']['typicalRangeHigh'])
     return float(typical)
 
-## define function get_record_max which makes the json output look pretty and easy to understand
 def get_record_max(obj):
     """Function takes api output from EA API and returns information about station."""
     recordmax = json.dumps(obj['items']['stageScale']['maxOnRecord']['value'])
     return float(recordmax)
 
-## define function set_gauge. Function calls API and then sets prometheus guage
 def set_gauge():
     """Function calls API, feeds to get_height and then sets prometheus guage."""
     ## get responses
@@ -61,10 +61,11 @@ def set_gauge():
     time.sleep(READ_INTERVAL * READ_UNITS)
 
 # initialise the gauges
-## call the API to get the station JSON, set variable to json response
+## call the API to get the station JSON
 initialise_gauge_station_response = rq.get(STATION_API, timeout=30)
+
 ## use get_station_name function to extract station name 'label'
-STATION_NAME = get_station_name(initialise_gauge_station_response.json)
+STATION_NAME = get_station_name(initialise_gauge_station_response.json())
 
 gauge_river_level = Gauge('river_level', f'River level at {STATION_NAME}')
 
